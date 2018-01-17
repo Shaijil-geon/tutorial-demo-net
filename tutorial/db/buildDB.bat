c288f7c4b0d274b68d7aa40b1031b5fb650be6ee
Shaijil-geon <shaijil.pb@gadgeon.com>
2018-01-17 17:21:58 +0530

    Connection string updated

--- tutorial/db/buildDB.bat
+++ tutorial/db/buildDB.bat
@@ -1,7 +1,7 @@
 @echo off
 SET DIR=%~d0%~p0%
 
-SET connstring="data source=SHAIJIL-LAPTOP\SQLEXPRESS;initial catalog=Northwind;integrated security=True"
+SET connstring="data source=SHAIJIL-LAPTOP\SQLEXPRESS;initial catalog=Northwind;Integrated Security=SSPI"
 SET repository.path="https://github.com/Shaijil-geon/tutorial-demo-net.git"
  
 "%DIR%..\..\packages\roundhouse.0.8.6\bin\rh.exe" /c=%connstring% /vf=../bin/tutorial.dll /r=%repository.path% /env=LOCAL /ni /simple
